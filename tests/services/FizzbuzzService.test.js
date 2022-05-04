6592134bd6cc569f96851ef53b8bbab23f949802
Moises Sanchez <moy5921@gmail.com>
2022-05-04 17:56:24 -0500

    agregando automatizacion desde carperta 4

---
+++ tests/services/FizzbuzzService.test.js
@@ -0,0 +1,23 @@
+const FizzbuzzService = require('../../lib/services/FizzbuzzService');
+
+
+describe('test for fizzbuzz service',() =>{
+
+    test('creating explorer objects to test',()=>{
+        const explorer1 = {name: "Explorer1", score: 1}
+        const explorer3 = {name: "Explorer3", score: 3}
+        const explorer5 = {name: "Explorer5", score: 5}
+        const explorer15 = {name: "Explorer15", score: 156}
+
+        expect(FizzbuzzService.applyValidationInExplorer(explorer1)).toStrictEqual({name: "Explorer1", score: 1, trick: 1})
+
+        expect(FizzbuzzService.applyValidationInExplorer(explorer3)).toStrictEqual({name: "Explorer3", score: 3, trick: "FIZZ"})
+
+        expect(FizzbuzzService.applyValidationInExplorer(explorer5)).toStrictEqual({name: "Explorer5", score: 5, trick: "BUZZ"})
+
+        expect(FizzbuzzService.applyValidationInExplorer(explorer15)).toStrictEqual({name: "Explorer15", score: 15, trick: "FIZZBUZZ"})
+
+
+    })
+
+})
